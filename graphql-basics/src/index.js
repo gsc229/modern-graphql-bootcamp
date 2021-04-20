f87aa55c095bc412080d52f5fee650fa0536a283
greg <gsc229@gmail.com>
2021-04-20 18:13:46 -0400

    set up basic typeDefs and resovers. Query working

---
+++ graphql-basics/src/index.js
@@ -0,0 +1,72 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+// The Five Scalar Types: String, Boolean, Int, Float, ID 
+// Non-Scalar Types: Objects and Arrays
+
+// Type Defs (schema):
+const typeDefs = `
+  type Query {
+    add(a: Float!, b: Float!): String
+    greeting(name: String, position: String): String
+    me: User!
+    post: Post!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
+  }
+`
+
+
+// Resolvers:
+const resolvers = {
+  Query: {
+    add(parent, args, ctx, info) {
+      console.log({args})
+      return `The answer is: ${args.a + args.b}`
+    },
+    greeting(parent, args, ctx, info){
+      if(args.name) return `Hello! My name is ${args.name} and I am a ${args.position}`
+      return 'Hi there. Wish I knew your name!'
+    },
+    me() {
+      return {
+        id: '123098',
+        name: 'Mike',
+        email: 'mike@example.com',
+        age: 28
+      }
+    },
+    post() {
+      return {
+        id: '092',
+        title: 'GraphQL 101',
+        body: 'This is a good resource',
+        published: false
+      }
+    }
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs: typeDefs,
+  resolvers: resolvers
+})
+
+server.start(()=> {
+  console.log('The server is up!')
+})
+
+
+
+
