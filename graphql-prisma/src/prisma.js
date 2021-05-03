e2b1e172aaa8540a44f8fb7213fb6a0ef396df27
greg <gsc229@gmail.com>
2021-05-03 17:08:03 -0400

    create update with exist check

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -9,9 +9,14 @@ const prisma = new Prisma({
 
 // prisma.query prisma.mutation prisma.subscription prisma.exists
 
-//1. Create a new post
-//2. Fetch all of the info abou the user
+
 const createPostForUser = async (authorId, data) => {
+
+  const userExists = await prisma.exists.User({
+    id: authorId
+  })
+
+  if(!userExists) throw new Error("No user with that id")
 
   const post = await prisma.mutation.createPost({
     data: {
@@ -22,48 +27,50 @@ const createPostForUser = async (authorId, data) => {
         }
       }
     }
-  }, '{ id }')
+  }, '{ id author { id name email posts { id title published } } }')
 
-  
-
-  const user = await prisma.query.users({
-    where: {
-      id: authorId
-    }
-  }, '{ id name email posts { id title published } }')
-
-  return user
-
+  return post.author
 }
 
 const updatePostForUser = async (postId, data) => {
+
+  const postExists = await prisma.exists.Post({
+    id: postId
+  })
+  
+  if(!postExists) throw new Error("Something went wrong updating post.")
 
   const updatedPost = await prisma.mutation.updatePost({
     where: {
       id: postId
     },
     data
-  }, '{ id author { id } }')
+  }, '{ id author { id name email posts { id title body published } } }')
 
-  if(!updatedPost) return new Error("Something went wrong updating post.")
-
-  const user = await prisma.query.users({
-    where: {
-      id: updatedPost.author.id
-    }
-  }, '{ id name email posts { id title body published } }')
-
-  return user
+  return updatedPost.author
 }
 
-updatePostForUser( "cko91wj2t00f209424tu72cfr" ,{
-  title: "Nancy likes pizza",
-  body: "In my last post I said hot dogs. I change my mind. It's pizza.",
-  published: false
-})
-.then(updatedInfo => {
-  console.log(JSON.stringify({updatedInfo}, null, 2))
+
+/* createPostForUser("cko8wgxg3000h0942tyzbr4rq", {
+    title: "Another one from Nancy",
+    body: "This one will work after the user exists check.",
+    published: true
+  }
+)
+.then(data => {
+  console.log(JSON.stringify({data}, null, 2))
 })
 .catch(error => {
-  console.log({error})
+  console.log(error.message)
+  console.log(JSON.stringify({error: error.message}))
+}) */
+
+/* updatePostForUser("cko938u6p00he0942ha4w9kvs", {
+  body: "Updating the last post from Nancy. "
 })
+.then(data => {
+  console.log(JSON.stringify({ data }, null, 2))
+})
+.catch(error => {
+  console.log({error: error.message})
+}) */
