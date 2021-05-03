e9a9ef5d2ca00270e2af1b31a908126e65655af8
greg <gsc229@gmail.com>
2021-05-03 16:08:45 -0400

    updatePost mutation

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -9,13 +9,52 @@ const prisma = new Prisma({
 
 // prisma.query prisma.mutation prisma.subscription prisma.exists
 
-prisma.query.users(null, '{ id name email }')
+/* prisma.query.users(null, '{ id name email posts { id title body }}')
 .then(data => {
-  console.log({ data })
-})
+  console.log(JSON.stringify({ data }, null, 2))
+}) */
 
 
-prisma.query.comments(null, '{ id text author { id name } }')
+/* prisma.query.comments(null, '{ id text author { id name } }')
 .then( commentsData => {
   console.log(JSON.stringify({commentsData}, null, 2))
+})
+ */
+
+/* prisma.mutation.createPost({
+  data: {
+    title: "This is a graphQL POST by Nancy",
+    body: "My name is Nancy and this is my first POST",
+    published: true,
+    author: {
+      connect: {
+        id: "cko8wgxg3000h0942tyzbr4rq"
+      }
+    }
+  }
+}, "{ id title body published }")
+.then(newPost => {
+  console.log(JSON.stringify({newPost}, null, 2))
+  return prisma.query.users(null, '{ id name email posts { id title body }}')
+})
+.then(data => {
+  console.log(JSON.stringify({ data }, null, 2))
+}) */
+
+prisma.mutation.updatePost({
+  data: {
+    title: "This is a SECOND graphQL POST by Nancy",
+    body: "This is acutally Nancy's second POST.",
+    published: true
+  },
+  where: {
+    id: "cko90vu0700db0942vnlms8jh"
+  }
+}, "{ id title body published }")
+.then(updatedPost => {
+  console.log(JSON.stringify({updatedPost}, null, 2))
+  return prisma.query.posts(null, '{ id title body }')
+})
+.then(data => {
+  console.log(JSON.stringify({ data }, null, 2))
 })
