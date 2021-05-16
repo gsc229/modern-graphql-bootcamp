88c965b742e448c080f7fd69267a8fa9f4fb9189
greg <gsc229@gmail.com>
2021-05-16 15:59:03 -0400

    restored relational data connections by removing the hard-coded relations in the resolvers (prisma autogenerated the relations for us. These hard-coded resolvers were overiding prisma

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -1,36 +1,50 @@
 const Query = {
 
   users(parent, args, { db, prisma }, info){
-    // the second parameter can take null, string or object --> you can throw the info object to the query.
-    console.log("QUERY: ", prisma.query)
-    return prisma.query.users(null, info)
+    const opArgs = {}
 
-    /* console.log({args, prisma})
     if(args.query){
-      console.log(`There was a query: ${JSON.stringify(args)}`)
-      return db.users.filter(user => {
-        if(user.email.toLowerCase() === args.query.toLowerCase()) return user
-
-        if(user.name.toLowerCase().includes(args.query.toLowerCase())) return user
-
-        if(user.age === args.query) return user
-
-      })
+      opArgs.where = {
+        OR: [
+          {name_contains: args.query},
+          {email_contains: args.query}
+        ]
+      }
     }
-    return db.users */
+    // the second parameter can take null, string or object --> you can throw the info object to the query.
+    return prisma.query.users(opArgs, info)
   },
+
   posts(parent, args, { db, prisma }, info){
-    console.log("=============================== HERE ARE THE POSTS: =========================== \n", JSON.stringify(prisma, null, 2))
-    //console.log(JSON.stringify({ prisma, info }, null, 2))
-    return prisma.query.posts(null, info)
-    /* if(args.query){
-      return posts.filter(post => post.title.includes(args.query) || post.body.includes(args.query))
+
+    const opArgs = {}
+
+    if(args.query){
+      opArgs.where = {
+        OR: [
+          { title_contains: args.query },
+          { body_contains: args.query }
+        ]
+      }
     }
-    return db.posts */
+
+    return prisma.query.posts(opArgs, info)
   },
+
   comments(parent, args, { db , prisma}, info){
-    return prisma.comments(null, info)
+
+    const opArgs = {}
+
+    if(args.query){
+      opArgs.where = {
+        text_contains: args.query
+      }
+    }
+
+
+    return prisma.query.comments(opArgs, info)
   },
+
   me() {
     return {
       id: '123098',
